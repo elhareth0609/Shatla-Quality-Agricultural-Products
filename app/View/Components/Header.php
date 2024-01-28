f36c938c148f53486b6a700c3a004405e798989e
elhareth0609 <elhareh0609@gmail.com>
2024-01-28 21:05:16 +0100

    first commit

---
+++ app/View/Components/Header.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Header extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return <<<'blade'
+<div>
+    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+</div>
+blade;
+    }
+}
---
+++ resources/views/content/home/components/notifications.blade.php
@@ -0,0 +1,82 @@
+
+<div class="overlay" data-overlay></div>
+
+<!--
+  - MODAL
+-->
+
+<div class="modal" data-modal>
+
+  <div class="modal-close-overlay" data-modal-overlay></div>
+
+  <div class="modal-content">
+
+    <button class="modal-close-btn" data-modal-close>
+      <ion-icon name="close-outline"></ion-icon>
+    </button>
+
+    <div class="newsletter-img">
+      <img src="{{ asset('assets/home/images/newsletter.png') }}" alt="subscribe newsletter" width="400" height="400">
+    </div>
+
+    <div class="newsletter">
+
+      <form action="#">
+
+        <div class="newsletter-header">
+
+          <h3 class="newsletter-title">Subscribe Newsletter.</h3>
+
+          <p class="newsletter-desc">
+            Subscribe the <b>Anon</b> to get latest products and discount update.
+          </p>
+
+        </div>
+
+        <input type="email" name="email" class="email-field" placeholder="Email Address" required>
+
+        <button type="submit" class="btn-newsletter">Subscribe</button>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+
+
+
+<!--
+  - NOTIFICATION TOAST
+-->
+
+<div class="notification-toast" data-toast>
+
+  <button class="toast-close-btn" data-toast-close>
+    <ion-icon name="close-outline"></ion-icon>
+  </button>
+
+  <div class="toast-banner">
+    <img src="{{ asset('assets/home/images/products/jewellery-1.jpg') }}" alt="Rose Gold Earrings" width="80" height="70">
+  </div>
+
+  <div class="toast-detail">
+
+    <p class="toast-message">
+      Someone in new just bought
+    </p>
+
+    <p class="toast-title">
+      Rose Gold Earrings
+    </p>
+
+    <p class="toast-meta">
+      <time datetime="PT2M">2 Minutes</time> ago
+    </p>
+
+  </div>
+
+</div>
